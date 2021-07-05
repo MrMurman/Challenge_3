b694daff0e07a4722de630d67a71cccd12ebe57d
MrMurman <storm2150@yandex.ru>
2021-07-05 12:39:33 +0300

    Initial Commit

---
+++ Challenge_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge_3
+//
+//  Created by Андрей Бородкин on 05.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
